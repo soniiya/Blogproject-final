f91552658eea4b7927af721625d7f6737711a360
Soniya Jaiswal <91732415+soniiya@users.noreply.github.com>
2021-12-26 13:37:41 +0530

    updated

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use(express.static("public"));
 
 app.use(express.static(path.join(__dirname, "client", "build")));
