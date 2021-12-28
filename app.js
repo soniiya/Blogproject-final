7abfe6441fe78644612e271c0f79a9bda3c9ea15
Soniya Jaiswal <91732415+soniiya@users.noreply.github.com>
2021-12-28 11:16:32 +0530

    againn

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const passport = require("passport");
 const session = require("express-session");
 const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require ('mongoose-findorcreate');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -89,8 +88,6 @@ app.get("/", function (req, res) {
   });
 });
 
-
-
 app.get("/profile", function (req, res) {
 
   Post.find({}, function (err, posts) {
@@ -120,7 +117,6 @@ app.post("/compose", function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-    //img: req.file.image
   });
 
   post.save(function (err) {
@@ -142,14 +138,6 @@ app.get("/posts/:postId", function(req, res){
     });
   });
 
-
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
-});
 
 app.get("/login", function (req, res) {
   res.render("login");
